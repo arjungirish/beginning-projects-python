8727cdd37f71e9de594e716473f07e73ab2ae91d
Arjun <arjungirish13@yahoo.com>
2019-12-27 10:52:55 -0500

    Added the integer parts to the branch

--- character_integer_counter.py
+++ character_integer_counter.py
@@ -1,12 +1,16 @@
 print("Hello, welcome to the character counter program.")
 the_string = input("Please enter the sentence which you want the characters counted for: ")
 spaces = 0
+numbers = 0
 for character in the_string:
   if character.isspace() == True:
     spaces += 1
- 
+  elif character.isnumeric() == True:
+    numbers += 1
 string_length = len(the_string)
 string_length = str(string_length)
 spaces = str(spaces)
+numbers = str(numbers)
 print("You have " + string_length + " characters in your input")
 print("You have " + spaces + " spaces in your input")
+print("You have " + numbers + " numbers in your input")
