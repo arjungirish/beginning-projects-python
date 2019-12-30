cf0910dd51f805a495cd427fff082002fa3a19d8
Arjun <arjungirish13@yahoo.com>
2019-12-30 10:37:40 -0500

    Made the code work

--- prime_number_detector.py
+++ prime_number_detector.py
@@ -3,19 +3,15 @@ print("Hello, welcome to the prime number detector!")
 number = input("Please enter a number: ")
 if number.isnumeric() == True:
   number = int(number)
-  if isinstance(number, int) and number > 0:
-    for  in range(number):
-      possible_factors = randint(0, number)
-      factors = 0
-      for values in possible_factors:
-        maybe_factor = number/values
-        if isinstance(maybe_factor, int):
-          factors += 1
-          if factors <= 2:
-            print("Your number is prime")
-          else:
-            print("Your number is not prime")
+  if isinstance(number, int) and number > 1:
+    for a_number in range(2,number):
+      if number % a_number == 0:
+        print("Your number", number, "is not a prime number")
+        break
+      else:
+        print("Your number", number, "is a prime number")
+        break
   else:
-    print("Please type in a positive number without decimals.")
+    print("Please type in a number greater than 1 and without decimals.")
 else:
-  print("Please type in a number.")
+  print("Please type in a number greater than 1 and without decimals.")
